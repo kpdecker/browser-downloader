47fb75fb0041852da8bddd3b34a56e37af78f507
kpdecker <kpdecker@gmail.com>
2016-12-18 09:55:53 -0600

    Add etag support to download

--- src/download.js
+++ src/download.js
@@ -1,10 +1,13 @@
-/* global fetch */
+/* global fetch, Headers */
 import 'isomorphic-fetch';
 
 import fs from 'fs';
+import mkdirp from 'mkdirp';
 import {basename, join} from 'path';
 
 export function download(url, destination) {
+  mkdirp.sync(destination);
+
   if (url.build) {
     return checkVersion(url, destination)
         .then((doIt) => {
@@ -17,20 +20,45 @@ export function download(url, destination) {
   }
 
   function download(url) {
-    return fetch(url)
+    let outputFile = join(destination, basename(url));
+    let headers;
+    try {
+      const currentETag = fs.readFileSync(`${outputFile}.etag`, 'utf8').trim();
+      headers = new Headers();
+      headers.append('if-none-match', currentETag);
+    } catch (err) {
+      /* NOP */
+    }
+
+    return fetch(url, {headers})
       .then((response) => {
+
+        url = decodeURI(response.url);
+        let outputFile = join(destination, basename(url));
+
+        if (response.status === 304) {
+          return outputFile;
+        }
         if (response.status !== 200) {
           return Promise.reject(`Failed to lookup url: ${response.status}`);
         }
 
-        url = decodeURI(response.url);
-        let outputFile = join(destination, basename(url));
+        let length = response.headers.get('content-length'),
+            etag = response.headers.get('etag');
 
         return new Promise((resolve, reject) => {
           let output = fs.createWriteStream(outputFile);
           response.body.pipe(output)
               .on('error', reject)
-              .on('finish', () => resolve(outputFile));
+              .on('finish', () => {
+                fs.writeFile(`${outputFile}.etag`, etag, (err) => {
+                  if (err) {
+                    reject(err);
+                  } else {
+                    resolve(outputFile);
+                  }
+                });
+              });
         });
       });
   }
